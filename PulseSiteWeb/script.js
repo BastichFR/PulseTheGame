426bfb77029060106985b456b3a128ff86eda1bb
Stanislas Dolata <stanislas.dolata@epita.fr>
2023-05-28 01:40:45 +0200

    Added dynamic light + Tweak to light

--- PulseSiteWeb/script.js
+++ PulseSiteWeb/script.js
@@ -1,11 +1,50 @@
-// Select the div with light id (there is only one div with this id per page)
+// Set an offset for scroll
 var offsetY = 0;
+// Select the div with light id (there is only one div with this id per page)
 var mouselight = document.getElementById('light');
 
 
-window.addEventListener('scroll', function () {
+// When the page is loaded
+// Replace all paragraphs by themselves but every words are in span
+// It aims to create a dynamic hover light effect
+document.addEventListener('DOMContentLoaded', function ()
+{
+	var arrayParagraph, strParagraph, i;
+	// Get all p element in the document (file) and convert it to an array
+	Array.from(document.getElementsByTagName('p')).forEach(function (pg)
+	{
+		// Split the string
+		arrayParagraph = pg.innerHTML.split(' ');
+		strParagraph = "";
+		i = 0;
+		while (i < arrayParagraph.length)
+		{
+			// Transform words into span words
+			// Detect link
+			if (arrayParagraph[i] == "<a")
+			{
+				strParagraph += "<span>" + arrayParagraph[i] + " " + arrayParagraph[i+1] + "</span> ";	
+				i++;
+			}
+			else
+			{
+				strParagraph += "<span>" + arrayParagraph[i] + "</span> ";
+			}
+
+			i++;
+		}
+
+		// Replace the current paragraph by itself with every words being span words
+		pg.innerHTML = strParagraph;
+	})
+});
+
+// Detect scroll
+window.addEventListener('scroll', function ()
+{
 	offsetY = window.scrollY;
-})
+});
+
 // document.documentElement is the root element
 // Add a listener on mouse move (keep track of)
 // Use an anonymous function that change css style of the element mouselight
